0158e814d137501c16ee39800ec3e61e3385f363
Alban <AlbanT@users.noreply.github.com>
2020-10-26 09:37:29 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,23 +1,16 @@
 
-//% color="#AA278D"
-//% groups="['Buttons', 'Joystick']"
-namespace Joystickbit {
-	export enum Buttons {
-		A = 0x1,
-		B = 0x2,
-		C = 0x3,
-		D = 0x4,
-		E = 0x5,
-		F = 0x6
-	}
+//% color="#AA278D" weight=100
+namespace hello {
+    //% block
+    export function helloWorld() {
+        basic.showString("Hello?")
+    }
 
-	function HelloWorldData(){
-		return "Hello World"
-	}
+    // note that Caml casing yields lower case
+    // block text with spaces
 
-	//% block
-    //% group="Buttons"
-	export function HelloWorld(){
-		basic.showString("Hello?")
-	}
+    //% block
+    export function camlCaseTwo() {
+
+    }
 }

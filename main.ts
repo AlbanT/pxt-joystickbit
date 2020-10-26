d3e02e7cd68082d1700701f1ad765c8c09925595
Alban <AlbanT@users.noreply.github.com>
2020-10-26 09:24:22 +0100

    first test

--- main.ts
+++ main.ts
@@ -1,3 +1,23 @@
-basic.forever(function () {
-	
-})
+
+//% color=190 weight=100 icon="\gamepad" block="Basic Blocks"
+//% groups="['Buttons', 'Joystick']"
+namespace Joystickbit {
+	export enum Buttons {
+		A = 0x1,
+		B = 0x2,
+		C = 0x3,
+		D = 0x4,
+		E = 0x5,
+		F = 0x6
+	}
+
+	function HelloWorldData(){
+		return "Hello World"
+	}
+
+	//% block
+    //% group="Buttons"
+	export function HelloWorld(): void{
+		basic.showString("Hello!")
+	}
+}
